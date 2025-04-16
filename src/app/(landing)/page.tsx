fff703c54be6a8e77eeaaab48dea29fec65ccf63
constructa-co <robert@constructa.co>
2025-04-16 14:48:22 +0400

    Remove duplicate landing page file

--- src/app/(landing)/page.tsx
+++
@@ -1,8 +0,0 @@
-                <div className={`absolute inset-0 transition-opacity duration-500 ${activeFeature === 3 ? 'opacity-100' : 'opacity-0'}`}>
-                  <Image
-                    src="/images/Cost Control 2.png"
-                    alt="Cost Control"
-                    fill
-                    className="object-contain"
-                  />
-                </div> 
